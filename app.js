34011305a7e8b3897ac034b8b25b773c0462e71d
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2016-04-13 15:14:16 -0400

    Set up html boilerplate

--- app.js
+++ app.js
@@ -2,20 +2,15 @@
 var express = require('express'),
     app = express(),
     morgan = require('morgan'),
-    path = require('path'),
-    swig = require('swig');
+    path = require('path');
 
 // Configure Express router
 app.use('/rx', express.static(path.join(__dirname, '/node_modules/rx/dist/')));
-app.use('/scripts', express.static(path.join(__dirname, '/js')));
-app.set('views', path.join(__dirname, '/html'));
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(morgan('dev'));
-app.listen(8000);
+app.listen(3000);
 
-// None of the data comes from our server
-// We only need to serve one page
-app.get('/', function(req, res) {
-    res.render('index');
-});
+// // Defined explicitly for logging purposes.
+// app.get('/', function(req, res) {
+//     res.sendfile('/public/index.html');
+// });
