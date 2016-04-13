df865e5afbb001ea1536e3a000ebb5ad07084585
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2016-04-13 10:48:17 -0400

    Set up simple back end

--- app.js
+++ app.js
@@ -2,13 +2,20 @@
 var express = require('express'),
     app = express(),
     morgan = require('morgan'),
-    path = require('path');
+    path = require('path'),
+    swig = require('swig');
 
 // Configure Express router
-app.use('/scripts', express.static(path.join(__dirname, '/node_modules/rx/dist/')));
+app.use('/rx', express.static(path.join(__dirname, '/node_modules/rx/dist/')));
+app.use('/scripts', express.static(path.join(__dirname, '/js')));
+app.set('views', path.join(__dirname, '/html'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 app.use(morgan('dev'));
 app.listen(8000);
 
+// None of the data comes from our server
+// We only need to serve one page
 app.get('/', function(req, res) {
-    res.send('<script src="scripts/rx.all.js"></script>');
+    res.render('index');
 });
