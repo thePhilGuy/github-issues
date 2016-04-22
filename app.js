e0e98ae3603726cd61747f3bb3fdc346d00d0067
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2016-04-22 12:52:15 -0400

    Display issues with style

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require('express'),
 
 // Configure Express router
 app.use('/rx', express.static(path.join(__dirname, '/node_modules/rx/dist/')));
+app.use('/semantic', express.static(path.join(__dirname, '/node_modules/semantic-ui/dist/')));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(morgan('dev'));
 app.listen(3000);
--- public/js/issues.js
+++ public/js/issues.js
@@ -10,6 +10,14 @@ function shorten(text, limit) {
     return short;
 }
 
+function styleLabel(label) {
+    var tag = '<div class="ui basic label" style="">' +
+            //   'text-shadow: white 0 0 10px;' +
+            //   'color: black;' +
+            //   'background-color: #' + label.color + ';">' +
+              label.name + '</div>';
+    return tag;
+}
 // ============== API Communication ============================================
 var requestStream = Rx.Observable.just('https://api.github.com/repos/npm/npm/issues');
 
@@ -20,11 +28,26 @@ var responseStream = requestStream
 
 responseStream.subscribe(function(response) {
     response.map(function(issue) {
-        var issueItem = '<div>' +
-                        '<h3>' + issue.title + '</h3>\n' +
-                        '<i>' + issue.labels + '</i>\n' +
-                        '<p>' + shorten(issue.body, 140) + '</p>\n' +
-                        '<p>' + '<img height="48" width="48" src=' + issue.user.avatar_url + ' />' + issue.user.login + '</p> </div>';
-        $('.content').append(issueItem);
+        if (issue.labels.length > 0) {
+            console.log(issue);
+            var issueItem ='<div class="item"> <div class="content">' +
+                           '<div class="ui red ribbon label"> #' + issue.number + '</div>' +
+                           '<a class="header">' + issue.title + '</a>';
+
+            if (issue.labels.length > 0) {
+                issueItem += '<div class="meta">';
+                for (var i = 0; i < issue.labels.length; i++) {
+                    issueItem += styleLabel(issue.labels[i]);
+                }
+                issueItem += '</div>';
+            }
+
+            issueItem +='<div class="description">' + shorten(issue.body, 140) + '</div>' +
+                        '<div class="extra">' +
+                        '<img src="' + issue.user.avatar_url + '" class="ui circular avatar image">' + issue.user.login +
+                        '</div> </div> </div>';
+
+            $('#issues_list').append(issueItem);
+        }
     });
 });
