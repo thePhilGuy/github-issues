5511c51f1367e8da8d09d2f6d7be857ee1563c41
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2016-04-23 01:59:17 -0400

    Getting pagination links

--- public/js/issues.js
+++ public/js/issues.js
@@ -12,8 +12,7 @@ function shorten(text, limit) {
 
 function styleLabel(label) {
     var tag = '<div class="ui basic label" style="' +
-            //   'text-shadow: black 0 0 10px;' +
-            //   'color: #' + label.color + ';' +
+              'box-shadow: 0 0 0.05em black;' +
               'border-color: #' + label.color + '!important;">' +
               label.name + '</div>';
     return tag;
@@ -23,31 +22,36 @@ var requestStream = Rx.Observable.just('https://api.github.com/repos/npm/npm/iss
 
 var responseStream = requestStream
   .flatMap(function(requestUrl) {
-    return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl));
+    return Rx.Observable.create(function (observer) {
+        jQuery.getJSON(requestUrl)
+        .done(function(response, status, jqXHR) {
+            observer.onNext(jqXHR);
+        })
+        .fail(function(jqXHR, status, error) { observer.onError(error); })
+        .always(function() { observer.onCompleted(); });
+    });
   });
 
 responseStream.subscribe(function(response) {
-    response.map(function(issue) {
+    console.log(response.getAllResponseHeaders());
+    response.responseJSON.map(function(issue) {
+        var issueItem ='<div class="item"> <div class="content">' +
+                       '<div class="ui ribbon label issue_number"> #' + issue.number + '</div>' +
+                       '<a class="header">' + issue.title + '</a>';
+
         if (issue.labels.length > 0) {
-            console.log(issue);
-            var issueItem ='<div class="item"> <div class="content">' +
-                           '<div class="ui red ribbon label"> #' + issue.number + '</div>' +
-                           '<a class="header">' + issue.title + '</a>';
-
-            if (issue.labels.length > 0) {
-                issueItem += '<div class="meta">';
-                for (var i = 0; i < issue.labels.length; i++) {
-                    issueItem += styleLabel(issue.labels[i]);
-                }
-                issueItem += '</div>';
+            issueItem += '<div class="meta">';
+            for (var i = 0; i < issue.labels.length; i++) {
+                issueItem += styleLabel(issue.labels[i]);
             }
-
-            issueItem +='<div class="description">' + shorten(issue.body, 140) + '</div>' +
-                        '<div class="extra">' +
-                        '<img src="' + issue.user.avatar_url + '" class="ui circular avatar image">' + issue.user.login +
-                        '</div> </div> </div>';
-
-            $('#issues_list').append(issueItem);
+            issueItem += '</div>';
         }
+
+        issueItem +='<div class="description">' + shorten(issue.body, 140) + '</div>' +
+                    '<div class="extra">' +
+                    '<img src="' + issue.user.avatar_url + '" class="ui circular avatar image">' + issue.user.login +
+                    '</div> </div> </div>';
+
+        $('#issues_list').append(issueItem);
     });
 });
